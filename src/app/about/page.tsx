2e22cf2524aa7bbe79cbc990054ce353bc607bad
Robert Baindourov <rbaindourov@gmail.com>
2025-02-17 13:37:25 -0800

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+// src/app/pages/about.tsx
+import type { NextPage } from "next";
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the about page.</p>
+        </div>
+    );
+};
+
+export default About;
